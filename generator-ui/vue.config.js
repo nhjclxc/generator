604ec99032ce951afb215a482469e269c697954b
LuoXianchao <nhjclxc@163.com>
2024-05-18 10:30:18 +0800

    修改

--- generator-ui/vue.config.js
+++ generator-ui/vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+  // publicPath: process.env.NODE_ENV === 'dev' ? '/' : '././'
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '././',
   // 关闭eslint校验
   lintOnSave: false,
   devServer: {
